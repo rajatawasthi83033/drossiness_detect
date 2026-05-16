2e321da808cbc8696744b816fe8e8111ee829763
rajatawasthi83033 <rajatawasthi83033@gmail.com>
2026-05-16 14:43:14 +0530

    use mediapipe libraby then detect face for better detection

--- drossiness_code.py
+++ drossiness_code.py
@@ -2,7 +2,7 @@ import cv2
 from tensorflow.keras.models import load_model
 import numpy as np
 import pygame
-
+import mediapipe as mp
 
 pygame.init()
 pygame.mixer.init()
@@ -12,131 +12,185 @@ eye_model=load_model("eye_detect96.keras")
 yawn_model=load_model("yawn_model82.keras")
 
 
-face_detect=cv2.CascadeClassifier(
-    cv2.data.haarcascades+"haarcascade_frontalface_default.xml"
+mp_face_mesh=mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(
+    static_image_mode=False,
+    max_num_faces=1,
+    refine_landmarks=True,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
 )
 
-eye_detect=cv2.CascadeClassifier(
-    cv2.data.haarcascades+"haarcascade_eye.xml"
-)
+def eye_process(eyes_landmarks,window_name):
+    eye_points=[]
+    padding=20
+    
+    for idx in eyes_landmarks:
+        landmark=face_landmarks.landmark[idx]
+                
+        x=int(landmark.x*w)
+        y=int(landmark.y*h)
+        
+        eye_points.append((x,y))
+        
+        cv2.circle(frame,(x,y),2,(0,255,00),-1)
+        
+    x_min=max(min([p[0] for p in eye_points]) - padding, 0)
+    x_max=min(max([p[0] for p in eye_points]) + padding, w)
+
+    y_min=max(min([p[1] for p in eye_points]) - padding, 0)
+    y_max=min(max([p[1] for p in eye_points]) + padding, h)
+    
+    cv2.rectangle(
+        frame,
+        (x_min, y_min),
+        (x_max, y_max),
+        (255,0,0),
+        2
+        )
+    
+    crop_eye=rgb_frame[y_min:y_max,x_min:x_max]
+    
+    
+    
+    if crop_eye.size==0:
+        return None
+    eye_resize=cv2.resize(crop_eye,(64,64))
+    cv2.imshow(window_name, eye_resize)
+    
+    eye_norm=eye_resize/255.0
+    
+    eye = np.expand_dims(eye_norm, axis=0)
+    
+    return eye
+
+def check_eye(predict):
+    pred_value = predict[0][0]
+    if pred_value>0.55:
+        return "opened"
+    else:
+        return "closed"
+    
+left_eye = [33,133,160,159,158,144]
+right_eye = [362,263,387,386,385,373]
+
 
 cap=cv2.VideoCapture(0)
-
-if not cap.isOpened():
-    print("Error:camera not open!")
-    exit()
-
-label="no eye"
-
 counter=0
 
+if not cap.isOpened():
+    print("camera is not openning.")
+    exit()
+    
+
 while True:
+    
     ret,frame=cap.read()
-
-    if ret:
-
-        eye_closed=False
-
-        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
-        faces=face_detect.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=2)
-
-        for x,y,w,h in faces:
-
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),thickness=2)
-
-            roi_gray=gray[y:y+h,x:x+w]
-
-            roi_color=frame[y:y+h,x:x+w]
-
-            eyes=eye_detect.detectMultiScale(roi_gray,scaleFactor=1.1,minNeighbors=5,minSize=(20,20))
-
-            for (ex,ey,ew,eh) in eyes:
-
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),thickness=3)
-
-                eye_crop=roi_color[ey:ey+eh,ex:ex+ew]
-
-                eye_crop=cv2.resize(eye_crop,(64,64))
-
-                eye_crop=eye_crop/255.0
-
-                eye_crop = np.reshape(eye_crop,(1,64,64,3))
-
-
-                pred=eye_model.predict(eye_crop,verbose=0)
-
-                
-
-                if pred>70:
-
-                    label="opened"
-
-                else:
-                    label="closed"
-                    eye_closed=True
-
-                
-                    
-
-        if eye_closed:
-            counter=min(counter+1,100)
-        else:
-            counter=0
-            pygame.mixer.music.stop()
-
-        cv2.putText(
-        frame,
-        f"Counter: {counter}",
-        (50,100),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (0,255,255),
-        2)
-
-
-        if counter>30:
-            cv2.putText(
-                frame,"Drossiness",(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2
-                )
-            if not pygame.mixer.music.get_busy():
-                pygame.mixer.music.play()
-
-        else:
-            cv2.putText(
-                frame,f"Status = {label}",(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2
-                )
+    
+    if not ret:
+        print("Exit")
+        break
+    
+    rgb_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    result=face_mesh.process(rgb_frame)
+    
+    if result.multi_face_landmarks:
+        
+        for face_landmarks in result.multi_face_landmarks:
             
-
+            left_class = "opened"
+            right_class = "opened"
             
+            
+            h,w,c=frame.shape
+            final_left_eye=eye_process(left_eye,"left eye")
+            
+            final_right_eye=eye_process(right_eye,"right_eye")
+            
+            if final_left_eye is not None:
+                
+                left_pred=eye_model.predict(final_left_eye,verbose=0)
+                
+                left_class=check_eye(left_pred)
+                
+                cv2.putText(frame,
+                            f"L: {left_class}",
+                            (20,100),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1,
+                            (0,255,0),
+                            2
+                            )
+            if final_right_eye is not None:
+                
+                right_pred=eye_model.predict(final_right_eye,verbose=0)
+                
+                right_class=check_eye(right_pred)
+                
+                
+                cv2.putText(
+                    frame,
+                    f"R: {right_class}",
+                    (20,150),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0,255,0),
+                    2
+                )
+                                
+            print(left_pred, right_pred)
+            
+            if right_class=="closed" and left_class=="closed":
+                counter+=1
+            
+            else:
+                counter=0
+    else:
+        counter=0
+        print("no face detect")
+        
+    if counter > 20:
 
-        cv2.imshow("eye_detect",frame)
+        if not pygame.mixer.music.get_busy():
 
-        if cv2.waitKey(1) & 0xff == ord("q"):
-            break
+            pygame.mixer.music.play()
 
     else:
-        print("not collect frame")
+
+        pygame.mixer.music.stop()
+    
+    cv2.putText(
+        frame,
+        f"Counter: {counter}",
+        (20,50),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0,0,255),
+        2
+    )
+    cv2.imshow("Final", frame)
+    
+    
+
+
+    
+    if cv2.waitKey(1) & 0xff==ord("q"):
+        print("closed by user")
         break
-
     
-    
-
-
-    
-               
-
-
-                
-
+  
 
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
-
-
+                
+                
+            
+                
+                
+            
+            
+                
+            
+            
     
